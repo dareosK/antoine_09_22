4c63de54af436219ec455c38f6a9c62c652da78f
Dareos Khalili <khalilidareos@gmail.com>
2022-09-27 09:15:28 +0200

    drops body from writings

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -2,4 +2,6 @@ class Writing < ApplicationRecord
   belongs_to :user
 
   has_one_attached :cover
+
+  has_rich_text :rich_body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_064026) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_071418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,7 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_064026) do
   create_table "writings", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title"
-    t.text "body"
     t.datetime "date"
     t.string "tagline"
     t.string "type"
