f4fa1f79e243a1bad735ced8c2ebce80cff43377
Dareos Khalili <khalilidareos@gmail.com>
2022-09-27 09:29:13 +0200

    new writing works with image upload and rich text

--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -4,4 +4,20 @@ class WritingsController < ApplicationController
   def index
     @writings = Writing.all.order(date: :desc)
   end
+
+  def create
+    @writing = Writing.new(writing_params)
+    @writing.user = current_user
+    if @writing.save!
+      redirect_to writing_path(@writing)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def writing_params
+    params.require(:writing).permit(:title, :tagline, :date, :rich_body, :cover)
+  end
 end
