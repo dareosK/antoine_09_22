5ffbbb0285726acb34a2a909360fbe274ec33137
Dareos Khalili <khalilidareos@gmail.com>
2022-12-25 12:07:08 +0100

    first push

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,31 +10,31 @@ require "open-uri"
 require 'faker'
 
 
-# puts "======👨‍🦰👳‍♀️ Creating Antoine"
-# one = User.create!(
-#     email: "one@mail.com",
-#     password: "123456",
-#     contact_mail: "hello@antoinechampenois.com",
-#     quotes: Faker::Quotes::Shakespeare.king_richard_iii_quote,
-#     bio: "Après une formation littéraire et philosophique classique, il intègre la Sorbonne en histoire de l’art avant de se consacrer pleinement à l’art contemporain et au commissariat d’exposition en 2019. Son champ d’investigation s’étend des nouvelles technologies et de l’ère digitale aux mécanismes de la pop culture. Cette spécialisation progressive et son goût pour l’écriture l’amènent à collaborer régulièrement avec des magazines ou directement auprès des artistes. Après un passage par TRAM, réseau d’art contemporain d’île-de-France, lui ayant permis de comprendre les enjeux propres au territoire francilien et de mettre à profit des publics son expertise engagée auprès de la jeune création, son parcours l’a mené au FRAC Lorraine (Metz)  pour la production d’expositions et la coordination d’édition.
+puts "======👨‍🦰👳‍♀️ Creating Antoine"
+one = User.create!(
+    email: "one@mail.com",
+    password: "123456",
+    contact_mail: "hello@antoinechampenois.com",
+    quotes: Faker::Quotes::Shakespeare.king_richard_iii_quote,
+    bio: "Après une formation littéraire et philosophique classique, il intègre la Sorbonne en histoire de l’art avant de se consacrer pleinement à l’art contemporain et au commissariat d’exposition en 2019. Son champ d’investigation s’étend des nouvelles technologies et de l’ère digitale aux mécanismes de la pop culture. Cette spécialisation progressive et son goût pour l’écriture l’amènent à collaborer régulièrement avec des magazines ou directement auprès des artistes. Après un passage par TRAM, réseau d’art contemporain d’île-de-France, lui ayant permis de comprendre les enjeux propres au territoire francilien et de mettre à profit des publics son expertise engagée auprès de la jeune création, son parcours l’a mené au FRAC Lorraine (Metz)  pour la production d’expositions et la coordination d’édition.
 
-#     Son attrait pour les pratiques collaboratives débordant la sphère de l’art contemporain l’ont conduit naturellement vers une équipe où la transdisciplinarité est une signature, il rejoint le studio Jérôme Sans en mars 2022.",
-#     admin: true
-#   )
+    Son attrait pour les pratiques collaboratives débordant la sphère de l’art contemporain l’ont conduit naturellement vers une équipe où la transdisciplinarité est une signature, il rejoint le studio Jérôme Sans en mars 2022.",
+    admin: true
+  )
 
-# puts "======📝 Creating Articles"
-# 10.times do
-#   writing = Writing.create(
-#     title: Faker::Book.title,
-#     rich_body: (Faker::Lorem.paragraphs(number: 360)).join,
-#     tagline: Faker::Quotes::Shakespeare.as_you_like_it_quote,
-#     date: Faker::Date.between(from: '2020-09-23', to: '2021-04-25'),
-#     user: one
-#     )
-#   writing_img = URI.open('https://source.unsplash.com/random')
-#   writing.cover.attach(io: writing_img, filename: 'random.png', content_type: 'image/png')
-# end
-# puts "======5 Articles created"
+puts "======📝 Creating Articles"
+10.times do
+  writing = Writing.create(
+    title: Faker::Book.title,
+    rich_body: (Faker::Lorem.paragraphs(number: 360)).join,
+    tagline: Faker::Quotes::Shakespeare.as_you_like_it_quote,
+    date: Faker::Date.between(from: '2020-09-23', to: '2021-04-25'),
+    user: one
+    )
+  writing_img = URI.open('https://source.unsplash.com/random')
+  writing.cover.attach(io: writing_img, filename: 'random.png', content_type: 'image/png')
+end
+puts "======5 Articles created"
 
 puts "======🖼 Creating 5 Projects"
 5.times do
