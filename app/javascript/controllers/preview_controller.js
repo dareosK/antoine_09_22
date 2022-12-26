00f808a6a9f27374f0a840260607e1e6475545e3
Dareos Khalili <khalilidareos@gmail.com>
2022-12-26 11:39:46 +0100

    image preview done

---
+++ app/javascript/controllers/preview_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['input', 'preview'];
+
+  connect() {
+    this.inputTarget.addEventListener('change', () => this.displayPreview());
+  }
+
+  displayPreview() {
+    console.log("test")
+    if (this.inputTarget.files && this.inputTarget.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        this.previewTarget.src = event.currentTarget.result;
+      }
+      reader.readAsDataURL(this.inputTarget.files[0])
+      this.previewTarget.classList.remove('hidden');
+    }
+  }
+}
