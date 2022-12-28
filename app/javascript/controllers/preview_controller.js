ca3a0530662e5fb1a261ad864b62ce9c4e3b84cb
Dareos Khalili <khalilidareos@gmail.com>
2022-12-28 12:44:13 +0100

    fixes bug on moodboard preview

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -4,52 +4,51 @@ export default class extends Controller {
   static targets = ['input', 'preview', 'list', 'previous'];
 
   connect() {
+    console.log("test")
+    // listen for a change on the upload button when the dashboard opens
     this.inputTarget.addEventListener('change', () => this.displayPreview());
   }
 
   displayPreview() {
-    console.log("test")
+    // check if there is at least one file about to be uploaded
     if (this.inputTarget.files && this.inputTarget.files[0]) {
+      // if so, clean up the previous moodboard, the image tag and the list
+      // NOTE on the html:
+      // 1. the image tag (this.previewTarget) will hold single images you try to upload
+      // 2. the ul#preview (this.listTarget) will holds multiple images you try to upload
+      // 3. the ul#previous (this.previousTarget) holds the previous moodboard
+      this.listTarget.innerHTML = ""
+      this.previousTarget.innerHTML = ""
+      this.previewTarget.src = ""
+
+      // check if you want to preview single of multiple image
+      // and call respective function
       if (this.inputTarget.files.length > 1) {
-        // cleanup the list
-        this.listTarget.innerHTML = ""
         this.displayMultiple(this.inputTarget.files)
       } else {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          this.previewTarget.src = event.currentTarget.result;
+        this.displaySingle(this.inputTarget.files)
         }
-        reader.readAsDataURL(this.inputTarget.files[0])
-        this.previewTarget.classList.remove('hidden');
-        if (this.previousTarget) {
-          console.log("inside single if statement")
-          this.previousTarget.innerHTML = "";
-        }
-      }
       }
     }
 
-  displayMultiple(fileList) {
+  displaySingle(fileList) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      this.previewTarget.src = event.currentTarget.result;
+    }
+    reader.readAsDataURL(fileList[0])
+  }
 
+  displayMultiple(fileList) {
     Array.from(fileList).forEach(file => {
       const reader = new FileReader();
       reader.onload = (event) => {
         const picFile = event.target;
         const div = document.createElement("div");
         const image = div.innerHTML = `<li class="item"><img class="thumbnail" src="${picFile.result}" title="${picFile.name}"/></li>`;
-        // console.log(image)
-        // console.log(this.listTarget)
-        this.previewTarget.classList.remove('hidden');
-        // this.previousTarget ? this.previousTarget.innerHTML = "" : continue
-        if (this.previousTarget) {
-          console.log("inside if statement")
-          this.previousTarget.innerHTML = "";
-        }
-        this.previewTarget.src = ""
         this.listTarget.insertAdjacentHTML("beforeend", image);
       }
       reader.readAsDataURL(file)
-      console.log("in displayMultiple")
      });
   }
 }
