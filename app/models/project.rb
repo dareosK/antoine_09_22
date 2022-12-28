b25f33b1e373f68049dd5e54ef59f00fa4df3bcb
Dareos Khalili <khalilidareos@gmail.com>
2022-12-28 13:05:15 +0100

    adds project_type to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,8 @@ class Project < ApplicationRecord
   has_one_attached :cover
   has_many_attached :photos
 
+  # validations
+  validates :project_type, inclusion: { in: %w(exhibition project) }
+
   has_rich_text :rich_body
 end
